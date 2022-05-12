ac64baf98ff38f4b8f6ec7e7131ace3301d12d95
Dagmoores <dagmoores@gmail.com>
2022-05-11 22:25:42 -0300

    atualização para forks

--- client/pi2_univesp_taquaral/src/pages/Main/index.jsx
+++ client/pi2_univesp_taquaral/src/pages/Main/index.jsx
@@ -1,17 +1,12 @@
 import { Link } from 'react-router-dom'
 
-//imports Bootstraps
-import Container  from 'react-bootstrap/Container'
-
 //import CSS
-import "../../App.css"
-
-
+import "./main.css"
 
 function Main() {
     return (
-        <body class="INDEX">         
-            <Link class="link" to="/"> <h1> Página inicial </h1></Link> 
+        <div className="INDEX">         
+            <Link className="link" to="/"> <h1> Página inicial </h1></Link> 
   
             <ul>
                 <li>
@@ -21,46 +16,46 @@ function Main() {
                     <Link to="/login"> Login </Link> 
                 </li>
             </ul>
-            <section class="u-align-center u-clearfix u-section-1" id="sec-e7af">
-      <div class="u-clearfix u-sheet u-sheet-1">
-	    <h1 class="u-text u-text-default u-text-2">RECICLA ELETRON</h1>
+            <section className="u-align-center u-clearfix u-section-1" id="sec-e7af">
+      <div className="u-clearfix u-sheet u-sheet-1">
+	    <h1 className="u-text u-text-default u-text-2">RECICLA ELETRON</h1>
 		
-        <h5 class="u-text u-text-default u-text-1"> ENCONTRE O PONTO DE COLETA MAIS PRÓXIMO DO SEU BAIRRO</h5>
-        <p class="u-text u-text-3"> A reciclagem é uma importante ferramenta para podermos amenizar o grande problema do acúmulo de lixo no mundo, mas também é importante nós reciclarmos nossas idéias, conceitos e valores para que sejamos seres humanos melhores e mais conscientes.</p>
+        <h5 className="u-text u-text-default u-text-1"> ENCONTRE O PONTO DE COLETA MAIS PRÓXIMO DO SEU BAIRRO</h5>
+        <p className="u-text u-text-3"> A reciclagem é uma importante ferramenta para podermos amenizar o grande problema do acúmulo de lixo no mundo, mas também é importante nós reciclarmos nossas idéias, conceitos e valores para que sejamos seres humanos melhores e mais conscientes.</p>
       </div>
     </section>
-    <div class="TabControl">
+    <div className="TabControl">
 	<div id="header">
-		<ul class="abas">
+		<ul className="abas">
 			<li>
-				<div class="aba">
+				<div className="aba">
 					<span>Um futuro melhor</span>
 				</div>
 			</li>
 			<li>
-				<div class="aba">
+				<div className="aba">
 					<span>Ajudando o mundo</span>
 				</div>
 			</li>
 			<li>
-				<div class="aba">
+				<div className="aba">
 					<span>Evitando poluição</span>
 				</div>
 			</li>
 			<li>
-				<div class="aba">
+				<div className="aba">
 					<span>Reciclando</span>
 				</div>
 			</li>
 		</ul>
 	</div>
 	<div id="content">
-		<div class="conteudo">
+		<div className="conteudo">
 		<b>DIREITOS RESERVADOS RECICLA ELETRON 2022</b>
 		</div>
 	</div>
 </div>
-        </body>
+        </div>
 
     )
 }
