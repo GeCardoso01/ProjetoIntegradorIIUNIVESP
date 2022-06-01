e800eb1b9cf4f0461837c33e196ba37f3bdcc850
Ge Cardoso <70989062+GeCardoso01@users.noreply.github.com>
2022-05-31 22:31:37 -0300

    site pronto para hospedagem

--- client/pi2_univesp_taquaral/src/pages/Cadastro/index.jsx
+++ client/pi2_univesp_taquaral/src/pages/Cadastro/index.jsx
@@ -1,11 +1,44 @@
 //import do componente Form
 import Formulario from "../../components/FormRegister"
 
+//react-router
+import { Link } from 'react-router-dom'
+
+//css
+import './cadastro.css'
 
 
 function paginaInicial() {
     return (
+<div>
+
+<div class="container-principal"></div>
+
+    <header>
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/sobre">Sobre</Link></li>
+                <li><Link to="/contato">Contato</Link></li>
+                <li><Link to="/cadastro">Cadastro</Link></li>
+                <li><Link to="/login">Login</Link></li>
+            </ul>
+        </nav>
+    </header>
+
+    <h1><a>Cadastre sua Empresa para ser um Ponto de Coleta de Recicláveis EletroEletrônicos !</a>
+    </h1>
+
+
         <h1><Formulario /></h1>
+        
+  
+    <h4><strong>Seus Dados estão Protegidos pela Lei LGPD </strong></h4>
+  
+       
+        </div>
+        
+        
     )
 }
 
--- client/pi2_univesp_taquaral/src/pages/Login/index.jsx
+++ client/pi2_univesp_taquaral/src/pages/Login/index.jsx
@@ -5,15 +5,42 @@ import FormLogin from "../../components/FormLogin"
 //import do arquivo css
 import './login.css'
 
+//react-router
+import {Link} from"react-router-dom"
 
 function Login() {
     return (
-        <Container md={{span:6, offset: 3 }}>
-            <h1>Login</h1>
-            <div className="formLoginComponent"><FormLogin /></div>  
-            
-        </Container>
-    )
+
+<div>    
+    <div  class='login'>
+
+        <div class="container-principal"></div>
+        </div>
+        <header>
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/sobre">Sobre</Link></li>
+                    <li><Link to="/contato">Contato</Link></li>
+                    <li><Link to="/cadastro">Cadastro</Link></li>
+                    <li><Link to="/login">Login</Link></li>
+                </ul>
+            </nav>
+        </header>
+            <section id="banner"> 
+            <img src="imagem/Recicla.jpg" alt="banner-topo" />
+    </section>
+        
+        
+
+            <h1><strong>Login</strong></h1><br></br>
+               
+        <div className="formLoginComponent"><FormLogin /></div>   
+        </div>
+        
+        
+        
+    ) 
 }
 
 export default Login
