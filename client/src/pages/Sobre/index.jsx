f3e01bd22ea305754a7e473aa3345b8e0b36bb4a
Daniel Moore <dagmoores@gmail.com>
2022-06-20 22:42:00 -0300

    mudancas titulos css e html gesiel

--- client/src/pages/Sobre/index.jsx
+++ client/src/pages/Sobre/index.jsx
@@ -12,21 +12,21 @@ function Sobre() {
 
 
           
-                <h1> Sobre a Ecocentro</h1>
+                <h1 className="gradient"> Ecocentro</h1>
                 <p><strong>Ecocentro - </strong> Servir como Ponto de Entrega Voluntária (PVE) pelos munícipes, de resíduos domiciliares recicláveis, tais como: Eletrônicos, Volumosos, Metais, Plásticos, Papel, Papelão, Lâmpadas Fluorescentes, Pilhas e Baterias, Roupas Inservíveis e Vidros.     Servir como Ponto de Entrega de Pneus Insersíveis por Usuários, Revendas e Oficinas, conforme Convênio entre a Prefeitura Municipal e ANIP-Associação Nacional das Indústrias de Pneumáticos para Transportes e Destinação Adequada ( Logística Reversa ).</p>
                 <img src="imagem/banner.jpg" alt="logo html5" />
                
 
-                <h1> Ecopontos, Descarte Conciente e Cidade mais Limpa</h1>
+                <h1 className="gradient"> Ecopontos, Descarte Conciente e Cidade mais Limpa</h1>
                 <p>De uma forma simples, <b> Ecoponto</b> Contribui para a Proteção da Saúde Pública e da Qualidade Ambiental. <b> Este é o objetivo dos Ecopontos </b>que além de estimular o Reaproveitamento e a reciclagem de materiais descartáveis, propicia à População um local específico para fazer a destinação do Lixo Reciclável acumulado em sua casa. </p>
                 <img src="imagem/RECICLA1.jpeg" alt="logo tecnologia" />
 
-                <h1> Projeto Recicla Electron </h1>
+                <h1 className="gradient"> Projeto Recicla Electron </h1>
                 <p><b> Recicla Electron</b> Foi desenvolvido por <b> Alunos </b> do Eixo da Computação da <b> UNIVESP (Universidade Virtual do Estado de São Paulo)</b>, do Polo da cidade de Taquaral-SP,para atender o trabalho Técnico Cíentifico voltado para problemas reais da sociedade em qual estamos inseridos.                                   Mais especificamente, a acessibilidade na web significa que pessoas com deficiência podem perceber, entender, navegar, interagir e construir para a web. E mais. Ela também beneficia outras pessoas, incluindo pessoas idosas com capacidades em mudanças devido ao envelhecimento - <i>w3c.br/ - acesso 09/2016</i></p>
                 <img src="imagem/Recicla.jpg" alt="logo acessibilidade" ></img>
 
                 <div class="footer-text">
-                <h3>Sobre a Ecocentro</h3>
+                <h3 className="gradient">Sobre a Ecocentro</h3>
                 <p>   Saiba o que é a Ecocentro e como a sua empresa pode ajudar. Haaa, não esqueça de cadastrar a sua Empresa para ser uma Ecoponto.</p>
             </div>
 
