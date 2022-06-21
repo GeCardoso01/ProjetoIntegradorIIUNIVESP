abd8209f1855a9be3097065c57f49f9509b146ed
Daniel Moore <dagmoores@gmail.com>
2022-06-20 22:35:02 -0300

    mudancas html e css atualizacao final

--- client/src/pages/Sobre/index.jsx
+++ client/src/pages/Sobre/index.jsx
@@ -11,58 +11,24 @@ function Sobre() {
             <div class="container-principal">
 
 
-                <br />
+          
+                <h1> Sobre a Ecocentro</h1>
+                <p><strong>Ecocentro - </strong> Servir como Ponto de Entrega Voluntária (PVE) pelos munícipes, de resíduos domiciliares recicláveis, tais como: Eletrônicos, Volumosos, Metais, Plásticos, Papel, Papelão, Lâmpadas Fluorescentes, Pilhas e Baterias, Roupas Inservíveis e Vidros.     Servir como Ponto de Entrega de Pneus Insersíveis por Usuários, Revendas e Oficinas, conforme Convênio entre a Prefeitura Municipal e ANIP-Associação Nacional das Indústrias de Pneumáticos para Transportes e Destinação Adequada ( Logística Reversa ).</p>
+                <img src="imagem/banner.jpg" alt="logo html5" />
+               
 
-                <div class="logo">
-                    <a href="index.html">
-                        <img src="imagem/R1.jpg" alt="logo site" />
-                    </a>
-                </div>
+                <h1> Ecopontos, Descarte Conciente e Cidade mais Limpa</h1>
+                <p>De uma forma simples, <b> Ecoponto</b> Contribui para a Proteção da Saúde Pública e da Qualidade Ambiental. <b> Este é o objetivo dos Ecopontos </b>que além de estimular o Reaproveitamento e a reciclagem de materiais descartáveis, propicia à População um local específico para fazer a destinação do Lixo Reciclável acumulado em sua casa. </p>
+                <img src="imagem/RECICLA1.jpeg" alt="logo tecnologia" />
 
-                <br />
-                <div id="openModal" class="modalDialog">
-                    <div>
-                        <a href="#close" class="close">X</a>
-                        <h1> Tudo Sobre Html</h1>
-                        <p><strong>HTML</strong> <i>(abreviação para a expressão inglesa HyperText Markup Language, que significa Linguagem de Marcação de Hipertexto)</i> é uma linguagem de marcação utilizada na construção de páginas na Web. Documentos HTML podem ser interpretados por navegadores. - <i> wikipedia.org - acesso 09/2016</i></p>
-                        <img src="imagem/banner.jpg" alt="logo html5" />
-                    </div>
+                <h1> Projeto Recicla Electron </h1>
+                <p><b> Recicla Electron</b> Foi desenvolvido por <b> Alunos </b> do Eixo da Computação da <b> UNIVESP (Universidade Virtual do Estado de São Paulo)</b>, do Polo da cidade de Taquaral-SP,para atender o trabalho Técnico Cíentifico voltado para problemas reais da sociedade em qual estamos inseridos.                                   Mais especificamente, a acessibilidade na web significa que pessoas com deficiência podem perceber, entender, navegar, interagir e construir para a web. E mais. Ela também beneficia outras pessoas, incluindo pessoas idosas com capacidades em mudanças devido ao envelhecimento - <i>w3c.br/ - acesso 09/2016</i></p>
+                <img src="imagem/Recicla.jpg" alt="logo acessibilidade" ></img>
 
-                </div>
-                <div id="openModal1" class="modalDialog">
-                    <div>
-                        <a href="#close" title="Close" class="close">X</a>
-                        <h1>Tecnologia - Realidade Aumentada</h1>
-                        <p>De uma forma simples, <b>Realidade Aumentada</b> é uma tecnologia que permite que o mundo virtual seja misturado ao real, possibilitando maior interação e abrindo uma nova dimensão na maneira como nós executamos tarefas, ou mesmo as que nósvincubimos nàs maquinas. Assim, se voçê pensava que objetos pulando para fora da tela eram elementos de filmes de ficção científica, está na hora de mudar seus conceitos. Aliás, o que acontece com a <b>Realidade Aumentada</b> é o contrário: voçê pulará para dentro do mundo virtual para interagir com objetos que só estão limitados à sua imaginação. - <i>tecmundo.com.br/realidade-aumentada - acesso 09/2016</i></p>
-                        <img src="imagem/html5.png" alt="logo tecnologia" />
-                    </div>
-                </div>
-
-                <div id="openModal2" class="modalDialog">
-                    <div>
-                        <a href="#close" title="Close" class="close">X</a>
-                        <h1>Acessibilidade</h1>
-                        <p><b>Acessibilidade</b> na web significa que pessoas com deficiência podem usar a web. Mais especificamente, a acessibilidade na web significa que pessoas com deficiência podem perceber, entender, navegar, interagir e construir para a web. E mais. Ela também beneficia outras pessoas, incluindo pessoas idosas com capacidades em mudanças devido ao envelhecimento - <i>w3c.br/ - acesso 09/2016</i></p>
-                        <img src="imagem/Acessibilidade" alt="logo acessibilidade" />
-                    </div>
-                </div>
-
-                <footer>
-                    <div class="footer-text">
-                        <h3>Tudo Sobre Html</h3>
-                        <p>Saiba o que é HTML,e como sua empresa pode usufruir de sites dinâmicos e funcionais. - <a href="#openModal">clique aqui</a>.</p>
-                    </div>
-
-                    <div class="footer-text">
-                        <h3>Tecnologia</h3>
-                        <p> Você sabe o que é realidade aumentada? Entenda como essa nova tecnologia funciona. - <a href="#openModal1">clique aqui</a>.</p>
-                    </div>
-
-                    <div class="footer-text">
-                        <h3>Acessibilidade</h3>
-                        <p>Torne seu site acessível utilizando 5 dicas indispensáveis para qualquer site. - <a href="#openModal2">clique aqui</a>.</p>
-                    </div>
-                </footer>
+                <div class="footer-text">
+                <h3>Sobre a Ecocentro</h3>
+                <p>   Saiba o que é a Ecocentro e como a sua empresa pode ajudar. Haaa, não esqueça de cadastrar a sua Empresa para ser uma Ecoponto.</p>
+            </div>
 
             </div> </div>
 
